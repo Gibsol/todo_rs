88768bd49e944c0afe766a7636c33d4a70235744
Gibsol <gibsol75@gmail.com>
2023-06-12 20:37:39 +0300

    Implement the test function in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello World")
+mod api;
+
+use actix_web::{App, HttpServer};
+use api::hello;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
