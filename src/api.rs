5e109e7ee55b704a4f34f86ce56f68641fb5d52d
Gibsol <gibsol75@gmail.com>
2023-06-30 16:08:22 +0300

    :sparkles: Impl the get_tasks function in api

--- src/api.rs
+++ src/api.rs
@@ -1,3 +1,4 @@
+use crate::db::Database;
 use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
 
 #[get("/")]
@@ -22,8 +23,11 @@ async fn hello() -> impl Responder {
 }
 
 #[get("/todos")]
-async fn get_all_todos() -> impl Responder {
-    HttpResponse::Ok().body("todos")
+async fn get_all_todos(db: web::Data<Database>) -> impl Responder {
+    match db.get_tasks().await {
+        Ok(todos) => HttpResponse::Ok().json(todos),
+        Err(_) => HttpResponse::InternalServerError().body("Something went wrong"),
+    }
 }
 
 #[post("/todos")]
@@ -32,7 +36,7 @@ async fn create_todo() -> impl Responder {
 }
 
 #[get("/todos/{id}")]
-async fn get_todo(path: web::Path<(u32)>) -> impl Responder {
+async fn get_todo(path: web::Path<u32>) -> impl Responder {
     let id = path.into_inner();
     HttpResponse::Ok().body("todos")
 }
