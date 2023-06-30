68943d8fe5136dc64e0a5b3f12d8b8df751eb21c
Gibsol <gibsol75@gmail.com>
2023-06-30 16:09:04 +0300

    :sparkles: Main function update
    declare necessary modules and implement the `/todos` service.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 mod api;
+mod db;
+mod models;
 
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 use api::hello;
+use api::*;
+use db::Database;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
+    let database = Database::connect()
         .await
+        .expect("Failed to connect to database");
+    let database_data = web::Data::new(database);
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(database_data.clone())
+            .service(hello)
+            .service(get_all_todos)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
