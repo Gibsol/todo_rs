b0d7246c83df11c0915c79942d934da85fdff3d4
Gibsol <gibsol75@gmail.com>
2023-07-01 12:54:26 +0300

    :sparkles: Add the get_todo function as a service to main

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod models;
 
 use actix_web::{web, App, HttpServer};
 use api::hello;
-use api::*;
+use api::{get_all_todos, get_todo};
 use db::Database;
 
 #[actix_web::main]
@@ -19,6 +19,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(database_data.clone())
             .service(hello)
             .service(get_all_todos)
+            .service(get_todo)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
