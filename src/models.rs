81bdcbf6b58406da3112a915d4a8218c5a8c2fda
Gibsol <gibsol75@gmail.com>
2023-07-01 13:33:31 +0300

    :construction: New request struct
    This is a hack atm. Will try to refactor this and use just the Todo
    struct.

--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,15 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Todo {
     pub id: i64,
     pub title: String,
     pub description: String,
     pub is_done: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TodoRequest {
+    pub title: String,
+    pub description: String,
 }
