5b06cb1af64616b0798b57d9a7a4a085f55bce65
Gibsol <gibsol75@gmail.com>
2023-07-01 13:32:52 +0300

    :sparkles: Create_todo implementation for Database

--- src/db.rs
+++ src/db.rs
@@ -55,6 +55,30 @@ impl Database {
         Ok(tasks)
     }
 
+    pub async fn create_task(&self, title: String, description: String) -> Result<Todo, Error> {
+        let row = self
+            .client
+            .query_one(
+                "INSERT INTO todos (title, description) VALUES ($1, $2) RETURNING *",
+                &[&title, &description],
+            )
+            .await?;
+
+        let id: i64 = row.get(0);
+        let title: String = row.get(1);
+        let description: String = row.get(2);
+        let is_done: bool = row.get(3);
+
+        let todo = Todo {
+            id,
+            title,
+            description,
+            is_done,
+        };
+
+        Ok(todo)
+    }
+
     pub async fn get_task_by_id(&self, id: i64) -> Result<Todo, Error> {
         let row = self
             .client
